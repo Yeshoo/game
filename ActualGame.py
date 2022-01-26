e7b8e7ef572292027e2bd937233d04306b9e9318
emailmeydrive <emailme.ydrive@gmail.com>
2022-01-26 14:36:00 +0200

    I did kind of a lot io think\

--- ActualGame.py
+++ ActualGame.py
@@ -5,16 +5,15 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
-select = 1
+select = 0
 
-games = 'C:\Users\pc\game\games'
+# The function that gets the names of all the games
+def get_dir_list():
+  return next(os.walk('.\games'))[1]
+print (get_dir_list())
 
-f = ( games,'r')
-
-for file in f:
-  print(file)
-
-pygame.init()
+# The list of all our games
+list_of_games = list((get_dir_list()))
 
 # Set the width and height of the screen [width, height]
 SIZE = (700, 500)
@@ -22,18 +21,23 @@ SCREEN = pygame.display.set_mode(SIZE)
 
 pygame.display.set_caption("BEANS ")
 
-SCREEN_SURFACE = pygame.image.load(r"C:\Users\pc\game\BEANS.jpg")
+SCREEN_SURFACE = pygame.image.load(r".\BEANS.jpg")
 
 # Loop until the user clicks the close button.
 done = False
-
+pygame.init()
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
-font = pygame.font.Font('freesansbold.ttf', 32)
-TEXTSURFACE = font.render('THESE ARE BEANS!!!!!!!!', False, (0, 0, 0))
-TEXTSURFACE2 = font.render('THERE ARE EVEN MORE BEANS!!!', False, BLACK)
-TEXTSURFACE3 = font.render('THE TEXT IS WHITE NOW', False, WHITE)
+font = pygame.font.SysFont('Comic Sans', 32)
+
+# Creating the list of all the text surfaces that will be used
+TEXT_SURFACES = []
+
+# Adding each a surface that will write each games name to the list
+for game in list_of_games:
+  TEXT_SURFACES.append(font.render(game, True, BLACK))
+
 # -------- Main Program Loop -----------
 while not done:
     # --- Main event loop
@@ -42,36 +46,25 @@ while not done:
             done = True
         # the selection mechanism which will be used to select a rectangle
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP and select > 1:
+            if event.key == pygame.K_UP and select > 0:
                 select = select - 1
-            if event.key == pygame.K_DOWN and select < 3:
+            if event.key == pygame.K_DOWN and select < len(TEXT_SURFACES) - 1:
                 select = select + 1
 
-    # --- Game logic should go here
-
-    # --- Screen-clearing code goes here
-
-    # Here, we clear the screen to white. Don't put other drawing commands
     # above this, or they will be erased with this command.
     SCREEN.blit(SCREEN_SURFACE, (0, 0))
-    # If you want a background image, replace this clear with blit'ing the
-    # background image.
-    SCREEN.blit(TEXTSURFACE, (0, 50))
-    SCREEN.blit(TEXTSURFACE2, (0, 100))
-    SCREEN.blit(TEXTSURFACE3, (170, 200))
-    if select == 1:
-      pygame.draw.rect(SCREEN_SURFACE, WHITE, pygame.Rect((0, 50), TEXTSURFACE.get_size()),2)
-      pygame.draw.rect(SCREEN_SURFACE, BLACK, pygame.Rect((0, 100), TEXTSURFACE2.get_size()),2)
-      pygame.draw.rect(SCREEN_SURFACE, BLACK, pygame.Rect((170, 200), TEXTSURFACE3.get_size()),2)
-    if select == 2:
-      pygame.draw.rect(SCREEN_SURFACE, BLACK, pygame.Rect((0, 50), TEXTSURFACE.get_size()),2)
-      pygame.draw.rect(SCREEN_SURFACE, WHITE, pygame.Rect((0, 100), TEXTSURFACE2.get_size()),2)
-      pygame.draw.rect(SCREEN_SURFACE, BLACK, pygame.Rect((170, 200), TEXTSURFACE3.get_size()),2)
-    if select == 3:
-      pygame.draw.rect(SCREEN_SURFACE, BLACK, pygame.Rect((0, 50), TEXTSURFACE.get_size()),2)
-      pygame.draw.rect(SCREEN_SURFACE, BLACK, pygame.Rect((0, 100), TEXTSURFACE2.get_size()),2)
-      pygame.draw.rect(SCREEN_SURFACE, WHITE, pygame.Rect((170, 200), TEXTSURFACE3.get_size()),2)
-    # --- Drawing code should go here
+    
+
+    # Drawing all the text surfaces and their borders according to the selected game
+    for i in range(len(TEXT_SURFACES)):
+      SCREEN_SURFACE.blit(TEXT_SURFACES[i], (0, 100*i))
+      if select == i:
+        pygame.draw.rect(SCREEN_SURFACE, WHITE, pygame.Rect((0, 100*i), TEXT_SURFACES[i].get_size()),2)  
+      else:
+        pygame.draw.rect(SCREEN_SURFACE, BLACK, pygame.Rect((0, 100*i), TEXT_SURFACES[i].get_size()),2)
+
+
+    
 
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
