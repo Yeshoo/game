6ce6870c0fd2bb85be6bc767d1351236c81c6b5c
emailmeydrive <emailme.ydrive@gmail.com>
2022-02-16 14:38:46 +0200

    yes

--- ActualGame.py
+++ ActualGame.py
@@ -1,3 +1,4 @@
+from numpy import size
 import pygame
 import os
 # Define some colors
@@ -51,9 +52,11 @@ SCREEN_HEIGHT = 700
 SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
 SCREEN = pygame.display.set_mode(SIZE)
 
-pygame.display.set_caption("BEANS ")
+pygame.display.set_caption("Selection Screen")
 
-SCREEN_SURFACE = pygame.image.load(r".\BEANS.jpg")
+SCREEN_SURFACE = pygame.image.load(r".\BK.jpg")
+
+SCREEN_SURFACE = pygame.transform.scale(SCREEN_SURFACE, SIZE)
 
 # Loop until the user clicks the close button.
 done = False
@@ -61,7 +64,7 @@ pygame.init()
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
-font = pygame.font.SysFont('Comic Sans', 32)
+font = pygame.font.Font('font.ttf', 32)
 Description_font = pygame.font.SysFont('Arial', 16)
 
 # Creating the list of all the text surfaces that will be used
@@ -73,12 +76,12 @@ GAME_IMAGE_SURFACES = []
 
 # Adding each a surface that will write each games name to the list
 for game in list_of_games:
-  TEXT_SURFACES.append(font.render(game, True, BLACK))
+  TEXT_SURFACES.append(font.render(game, True, WHITE))
 
 for desc in list_of_game_text_root:
   f = open(desc, "rt")
   text = f.read()
-  TEXT_DESCRIPTION_SURFACES.append(Description_font.render(text, True, BLACK))
+  TEXT_DESCRIPTION_SURFACES.append(Description_font.render(text, True, WHITE))
 
 for img_root in list_of_game_image_roots:
  GAME_IMAGE_SURFACES.append(pygame.image.load(img_root))
@@ -98,19 +101,21 @@ while not done:
                 select = select + 1
             if event.key == pygame.K_SPACE:
                os.system("python ." + list_of_game_python_script[select][1:])
-    SCREEN.blit(SCREEN_SURFACE, (0, 0))\
+    SCREEN.blit(SCREEN_SURFACE, (0, 0))
     
     
 
     # Drawing all the text surfaces and their borders according to the selected game
     for i in range(len(TEXT_SURFACES)):
-      SCREEN_SURFACE.blit(TEXT_SURFACES[i], (0, 100*i))
+      SCREEN_SURFACE.blit(TEXT_SURFACES[i], (70, 100*i + 100))
       if select == i:
-        pygame.draw.rect(SCREEN_SURFACE, WHITE, pygame.Rect((0, 100*i), TEXT_SURFACES[i].get_size()),2)  
+        [h, w] = TEXT_SURFACES[i].get_size()
+        pygame.draw.rect(SCREEN_SURFACE, WHITE, pygame.Rect((70, 100*i + 100), (h, w+3)),2)  
       else:
-        pygame.draw.rect(SCREEN_SURFACE, BLACK, pygame.Rect((0, 100*i), TEXT_SURFACES[i].get_size()),2)
+        [h, w] = TEXT_SURFACES[i].get_size()
+        pygame.draw.rect(SCREEN_SURFACE, BLACK, pygame.Rect((70, 100*i + 100), (h, w+3)),2)
     SCREEN.blit(TEXT_DESCRIPTION_SURFACES[select], (SCREEN_WIDTH/2,50))
-    SCREEN.blit(pygame.transform.scale(GAME_IMAGE_SURFACES[select], (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)),(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+    SCREEN.blit(pygame.transform.scale(GAME_IMAGE_SURFACES[select], (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)),((SCREEN_WIDTH/2) - 70, (SCREEN_HEIGHT/2) - 50))
 
 
     # --- Go ahead and update the screen with what we've drawn.
