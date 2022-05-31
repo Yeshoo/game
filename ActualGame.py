8a982c6036d123ee86775a7f358e6db3034e392e
Yeshoo <97167150+Yeshoo@users.noreply.github.com>
2022-05-31 11:03:03 +0100

    Update ActualGame.py

--- ActualGame.py
+++ ActualGame.py
@@ -1,28 +1,54 @@
 from numpy import size
 import pygame
 import os
+import playsound
+import cv2
+import  mediapipe as mp
 # Define some colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 select = 0
+mp_Hands = mp.solutions.hands
+hands = mp_Hands.Hands(max_num_hands=1)
+mpDraw = mp.solutions.drawing_utils
+
+orgX = 50, 80
+orgY = 50, 120
+orgHandPos = 50, 160
+orgTEXT = 50, 40
+vid = cv2.VideoCapture(0)
+neutral = True
+previous_frame_neutral = True
+
+# The functions that identify the shape the user makes with his hand in order to command the screen
+def is_fist(Middle_line, Finger1, Finger2, Finger3, Finger4):
+    if Middle_line < Finger1 and Middle_line < Finger2 and Middle_line < Finger3 and Middle_line < Finger4:
+        return True
+    else:
+        return False
+def is_certain_shape(Middle_line, Above1, Above2, Below1, Below2):
+    if Middle_line > Above1 and Middle_line > Above2 and Middle_line < Below1 and Middle_line < Below2:
+        return True
+    else:
+        return False
+
 
 pygame.mixer.init
 pygame.joystick.init()
-joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
-
-cwd = '.\games'
+joysticks = [pygame.joystick.Joystick(i).init() for i in range(pygame.joystick.get_count())]
+cwd = 'games'
 
 # The functions that get the names and roots of all the games
 def get_dir_list():
   return next(os.walk(cwd))[1]
 
 def get_dir_root_list():
-  roots = []
-  for game in next(os.walk(cwd))[1]:
-    roots.append(os.path.join(cwd, game))
-  return roots
+    roots = []
+    for game in next(os.walk(cwd))[1]:
+        roots.append(os.path.join(cwd, game))
+    return roots
 
 def get_game_text_roots():
   roots = []
@@ -51,14 +77,14 @@ list_of_game_image_roots = list((get_game_image_roots()))
 
 # Set the width and height of the screen [width, height]
 
-SCREEN_WIDTH = 1300
-SCREEN_HEIGHT = 700
+SCREEN_WIDTH = 1680
+SCREEN_HEIGHT = 1000
 SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
 SCREEN = pygame.display.set_mode(SIZE)
 
 pygame.display.set_caption("Selection Screen")
 
-SCREEN_SURFACE = pygame.image.load(r".\BK.jpg")
+SCREEN_SURFACE = pygame.image.load(r"BK.jpg")
 
 SCREEN_SURFACE = pygame.transform.scale(SCREEN_SURFACE, SIZE)
 
@@ -94,6 +120,47 @@ for img_root in list_of_game_image_roots:
 # -------- Main Program Loop -----------
 while not Done:
     # --- Main event loop
+    ret, frame = vid.read()
+    RGB_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = hands.process(RGB_frame)
+    multiLandMarks = results.multi_hand_landmarks
+    
+    if multiLandMarks:
+        for h in multiLandMarks:
+            mpDraw.draw_landmarks(frame, h, mp_Hands.HAND_CONNECTIONS)
+
+        index_finger_tip_y = multiLandMarks[0].landmark[8].y
+        middle_finger_tip_y = multiLandMarks[0].landmark[12].y
+        ring_finger_tip_y = multiLandMarks[0].landmark[16].y
+        pinky_tip_y = multiLandMarks[0].landmark[20].y
+        
+        index_finger_base_y = multiLandMarks[0].landmark[6].y
+        
+        hand_center_position_y = multiLandMarks[0].landmark[9].y
+
+        neutral = not is_fist(index_finger_base_y, index_finger_tip_y, middle_finger_tip_y, ring_finger_tip_y, pinky_tip_y)
+        
+        enter_game = is_certain_shape(index_finger_base_y, index_finger_tip_y, pinky_tip_y, middle_finger_tip_y, ring_finger_tip_y)
+
+        if hand_center_position_y < 0.5:
+            hand_height = "high"
+        else:
+            hand_height = "low"
+        
+        if enter_game and not previous_frame_entry and previous_frame_neutral:
+            playsound("./Selected.mp3")
+            os.chdir(list_of_game_roots[select])
+            os.system("python main.py")
+            os.chdir("../..")
+        elif not neutral and hand_height == "high" and previous_frame_neutral:
+                select = select - 1
+                playsound("./Change.mp3")
+        elif not neutral and hand_height == "low" and previous_frame_neutral:
+                select = select + 1
+                playsound("./Change.mp3")
+        
+        previous_frame_neutral = neutral
+        previous_frame_entry = enter_game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             Done = True
@@ -101,38 +168,42 @@ while not Done:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP and select > 0:
                 select = select - 1
-                pygame.mixer.Sound("Change.mp3").play()
+                playsound("./Change.mp3")
             if event.key == pygame.K_DOWN and select < len(TEXT_SURFACES) - 1:
                 select = select + 1
-                pygame.mixer.Sound("Change.mp3").play()
+                playsound("./Change.mp3")
             if event.key == pygame.K_SPACE:
-               pygame.mixer.Sound("Selected.mp3").play()
-               os.system("python ." + list_of_game_python_script[select][1:])
-        elif event.type == pygame.JOYBUTTONDOWN:
-           pygame.mixer.Sound("Selected.mp3").play()
-           os.system("python ." + list_of_game_python_script[select][1:])
-        elif event.type == pygame.JOYAXISMOTION:
-            if event.axis == 1 and event.value <= -0.1:
-                  select = select + 1
-                  pygame.mixer.Sound("Change.mp3").play()
-            if event.axis == 1 and event.value > 0:
-                  select = select - 1
-                  pygame.mixer.Sound("Change.mp3").play()
+                playsound("./Selected.mp3")
+                os.chdir(list_of_game_roots[select])
+                os.system("python main.py")
+                os.chdir("../..")
+        if event.type == pygame.JOYBUTTONDOWN:
+            playsound("./Selected.mp3")
+            os.chdir(list_of_game_roots[select])
+            os.system("python main.py")
+            os.chdir("../..")
+        if event.type == pygame.JOYAXISMOTION:
+            if event.axis == 1 and event.value <= -0.1 and select < len(TEXT_SURFACES) - 1:
+                select = select + 1
+                playsound("./Change.mp3")
+            if event.axis == 1 and event.value > 0 and select > 0:
+                select = select - 1
+                playsound("./Change.mp3")
     SCREEN.blit(SCREEN_SURFACE, (0, 0))
     
     
 
     # Drawing all the text surfaces and their borders according to the selected game
     for i in range(len(TEXT_SURFACES)):
-      SCREEN_SURFACE.blit(TEXT_SURFACES[i], (70, 100*i + 100))
+      SCREEN_SURFACE.blit(TEXT_SURFACES[i], (85, 100*i + 100))
       if select == i:
         [h, w] = TEXT_SURFACES[i].get_size()
-        pygame.draw.rect(SCREEN_SURFACE, WHITE, pygame.Rect((70, 100*i + 100), (h, w+3)),2)  
+        pygame.draw.rect(SCREEN_SURFACE, WHITE, pygame.Rect((85, 100*i + 100), (h, w+3)),2)  
       else:
         [h, w] = TEXT_SURFACES[i].get_size()
-        pygame.draw.rect(SCREEN_SURFACE, BLACK, pygame.Rect((70, 100*i + 100), (h, w+3)),2)
-    SCREEN.blit(TEXT_DESCRIPTION_SURFACES[select], (SCREEN_WIDTH/2,50))
-    SCREEN.blit(pygame.transform.scale(GAME_IMAGE_SURFACES[select], (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)),((SCREEN_WIDTH/2) - 70, (SCREEN_HEIGHT/2) - 50))
+        pygame.draw.rect(SCREEN_SURFACE, BLACK, pygame.Rect((85, 100*i + 100), (h, w+3)),2)
+    SCREEN.blit(TEXT_DESCRIPTION_SURFACES[select], (SCREEN_WIDTH/2,80))
+    SCREEN.blit(pygame.transform.scale(GAME_IMAGE_SURFACES[select], (SCREEN_WIDTH//2, SCREEN_HEIGHT//2)),((SCREEN_WIDTH//2) - 84, (SCREEN_HEIGHT//2) - 65))
 
 
     # --- Go ahead and update the screen with what we've drawn.
@@ -142,4 +213,6 @@ while not Done:
     clock.tick(60)
 
 # Close the window and quit.
+vid.release()
+cv2.destroyAllWindows()
 pygame.quit()
